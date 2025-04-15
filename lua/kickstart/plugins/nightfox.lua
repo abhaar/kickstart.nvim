b2d81ada6cbeb47b531abe54dfc25be47b2de9f8
Abhaar Gupta <abhaar.gupta@anchorlabs.com>
2025-04-15 21:41:37 +0000

    wip

--- lua/kickstart/plugins/nightfox.lua
+++ lua/kickstart/plugins/nightfox.lua
@@ -3,16 +3,14 @@ return { -- You can easily change to a different colorscheme.
   -- change the command in the config to whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'navarasu/onedark.nvim',
+  'EdenEast/nightfox.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
-    require('onedark').setup {
-      styles = 'darker',
-    }
+    require('nightfox').setup {}
 
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'onedark'
+    -- vim.cmd.colorscheme 'carbonfox'
   end,
 }
