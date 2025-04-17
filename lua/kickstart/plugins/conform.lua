55ab4783806bfdecaf8f7051e6944044d08b0ed4
abhaar <abhaar@gmail.com>
2025-04-16 20:14:38 -0400

    buf formatter

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -31,6 +31,7 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { 'stylua' },
       go = { 'gofmt' },
+      proto = { 'buf' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
