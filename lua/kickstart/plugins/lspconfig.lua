cce935af142cc60cdd1b1c782a4f15e096422d35
abhaar <abhaar@gmail.com>
2025-04-27 21:12:02 -0400

    enable python lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -222,7 +222,7 @@ return {
           filetypes = { 'proto' },
           root_dir = require('lspconfig.util').root_pattern('buf.yaml', '.git'),
         },
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
