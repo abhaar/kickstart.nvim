33d49612dd924b5ebc759c5bf6e0db7f32a80039
Abhaar Gupta <abhaar.gupta@anchorlabs.com>
2025-05-13 22:57:01 +0000

    add copilot chat

--- lua/kickstart/plugins/copilot.lua
+++ lua/kickstart/plugins/copilot.lua
@@ -1,3 +1,14 @@
 return {
-  'github/copilot.vim',
+  {
+    'github/copilot.vim',
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+      { 'github/copilot.vim' },
+    },
+    build = 'make tiktoken',
+    opts = {},
+  },
 }
